e3df78442ee404e23a4683d4973bef29770f5609
Matt Dean <matt.dean.001@gmail.com>
2017-08-23 23:12:11 +0100

    Configure ESLint to support ES2017

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,17 +2,25 @@ module.exports = {
   extends: "eslint:recommended",
   rules: {
     "no-console": 0,
+    "object-curly-spacing": ["error", "always"],
+    "comma-dangle": [
+      "error",
+      {
+        arrays: "always-multiline",
+        objects: "always-multiline",
+        imports: "always-multiline",
+        exports: "always-multiline",
+        functions: "ignore",
+      },
+    ],
     quotes: ["error", "backtick"],
-    semi: ["error", "never"]
+    semi: ["error", "never"],
   },
   env: {
     node: true,
-    es6: true
   },
   parserOptions: {
-    ecmaFeatures: {
-      modules: "true"
-    },
-    sourceType: "module"
-  }
+    ecmaVersion: 2017,
+    sourceType: "module",
+  },
 }
