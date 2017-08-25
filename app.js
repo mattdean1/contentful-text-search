c65f642e25eebb2957de9a076bec2f3e18d82fe2
Matt Dean <matt.dean.001@gmail.com>
2017-08-25 15:17:57 +0100

    Add function to resolve references in CF entries

--- app.js
+++ app.js
@@ -9,5 +9,6 @@ checkEnv([`CF_SPACE_ID`, `CF_ACCESS_TOKEN`])
 
 const cf = new Contentful(process.env.CF_SPACE_ID, process.env.CF_ACCESS_TOKEN)
 cf.getEntries().then(entries => {
-  console.log(entries)
+  console.log(JSON.stringify(cf.resolve(entries), null, 2))
+  process.exit(0)
 })
--- src/contentful.js
+++ src/contentful.js
@@ -55,4 +55,46 @@ module.exports = class Contentful {
       throw new Error(err)
     }
   }
+
+  // Recursively resolve references in an array of contentful entries
+  // TODO: add logging, should this return a promise?
+  resolve(content, entriesMap) {
+    if (!entriesMap) {
+      entriesMap = this.createEntriesMap(content)
+    }
+
+    // content is an array
+    if (Array.isArray(content)) {
+      return content.map(x => this.resolve(x, entriesMap))
+    }
+    // content is an entry with fields
+    if (content.sys && content.sys.type === `Entry`) {
+      Object.keys(content.fields).forEach(fieldName => {
+        // TODO: support multiple locales - group fields by locale?
+        content.fields[fieldName][`en-US`] = this.resolve(
+          content.fields[fieldName][`en-US`],
+          entriesMap
+        )
+      })
+      return content
+    }
+    // Content is a reference
+    if (
+      content.sys &&
+      content.sys.type === `Link` &&
+      content.sys.linkType === `Entry`
+    ) {
+      return this.resolve(entriesMap[content.sys.id], entriesMap)
+    }
+    // content is a value
+    return content
+  }
+
+  // create an object with content ID as keys
+  createEntriesMap(entries) {
+    return entries.reduce(
+      (accu, entry) => Object.assign(accu, { [entry.sys.id]: entry }),
+      {}
+    )
+  }
 }
