5f2371f8d2d1d555c902515298a0c5523735a065
Matt Dean <matt.dean.001@gmail.com>
2017-09-02 18:57:13 +0100

    Add tests describing public API

--- test/index.js
+++ test/index.js
@@ -1,10 +1,48 @@
 const assert = require(`assert`) // node.js core module
 
+const ContentfulTextSearch = require(`../`)
 const transform = require(`../src/transform`)
 
 const testData = require(`./test-data`)
 
 describe(`Package`, () => {
+  describe(`Public API`, () => {
+    let instance
+
+    beforeEach(() => {
+      // Create a new class instance before every test.
+      instance = new ContentfulTextSearch({
+        space: `fakestring`,
+        token: `fakestring`,
+        elasticLogLevel: `error`,
+      })
+    })
+
+    it(`should have a query method`, () => {
+      assert.equal(typeof instance, `object`)
+      assert.equal(typeof instance.query, `function`)
+    })
+    it(`should contain an indexer class`, () => {
+      assert.equal(typeof instance, `object`)
+      assert.equal(typeof instance.indexer, `object`)
+    })
+
+    describe(`Indexer`, () => {
+      it(`should have a fullReindex method`, () => {
+        assert.equal(typeof instance.indexer.fullReindex, `function`)
+      })
+      it(`should have a reindexContent method`, () => {
+        assert.equal(typeof instance.indexer.reindexContent, `function`)
+      })
+      it(`should have a deleteAllIndices method`, () => {
+        assert.equal(typeof instance.indexer.deleteAllIndices, `function`)
+      })
+      it(`should have a reindexContent method`, () => {
+        assert.equal(typeof instance.indexer.reindexContent, `function`)
+      })
+    })
+  })
+
   describe(`Transformations`, () => {
     it(`should reduce content types`, () => {
       assert.deepEqual(
